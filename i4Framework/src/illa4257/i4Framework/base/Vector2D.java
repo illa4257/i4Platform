c3d2549a356aabad9860cc83ae616aa4d35d229d
illa4257 <48959534+illa4257@users.noreply.github.com>
2025-01-04 15:00:03 -0800

    Implements equals() and toString() for Vector2D.

--- i4Framework/src/illa4257/i4Framework/base/Vector2D.java
+++ i4Framework/src/illa4257/i4Framework/base/Vector2D.java
@@ -13,4 +13,18 @@ public class Vector2D {
     public static Vector2D fromSize(final Rectangle2D rectangle2D) {
         return new Vector2D((float) rectangle2D.getWidth(), (float) rectangle2D.getHeight());
     }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (o instanceof Vector2D) {
+            final Vector2D v = (Vector2D) o;
+            return x == v.x && y == v.y;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Vector2D{x=" + x + ", y=" + y + "}";
+    }
 }
