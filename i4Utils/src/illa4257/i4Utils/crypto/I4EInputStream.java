6ed716de5aae0b36c549f7c8d252dd4fd5054432
illa4257 <48959534+illa4257@users.noreply.github.com>
2025-01-10 13:18:41 -0800

    New Encryption method

---
+++ i4Utils/src/illa4257/i4Utils/crypto/I4EInputStream.java
@@ -0,0 +1,57 @@
+package illa4257.i4Utils.crypto;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class I4EInputStream extends InputStream {
+    private final InputStream stream;
+    private final byte[] sign;
+    private int signIndex;
+    private int status = 0, len = 0;
+
+    public I4EInputStream(final InputStream stream, final byte[] sign, final int signIndex) {
+        this.stream = stream;
+        this.sign = sign;
+        this.signIndex = signIndex;
+    }
+
+    public I4EInputStream(final InputStream stream, final byte[] sign) { this(stream, sign, 0); }
+
+    private int internalRead() throws IOException {
+        if (signIndex >= sign.length)
+            signIndex = 0;
+        final int r = stream.read();
+        if (r == -1)
+            return -1;
+        return ((byte) r - sign[signIndex++]) & 0xFF;
+    }
+
+    private int readInt() throws IOException {
+        return (internalRead() << 24) + (internalRead() << 16) + (internalRead() << 8) + internalRead();
+    }
+
+    private void status() throws IOException {
+        if (status == 1) {
+            if (len > 0)
+                return;
+            stream.skip(readInt());
+        }
+        stream.skip(readInt());
+        len = readInt();
+        status = 1;
+    }
+
+    @Override
+    public int read() throws IOException {
+        status();
+        final int r = internalRead();
+        if (r > -1)
+            len--;
+        return r;
+    }
+
+    @Override
+    public void close() throws IOException {
+        stream.close();
+    }
+}
---
+++ i4Utils/src/illa4257/i4Utils/crypto/I4EOutputStream.java
@@ -0,0 +1,100 @@
+package illa4257.i4Utils.crypto;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.security.SecureRandom;
+import java.util.Random;
+
+public class I4EOutputStream extends OutputStream {
+    private final int dataMaxLength;
+    private final Random randomizer;
+    private final OutputStream stream;
+    private final byte[] sign, randomData;
+    private int signIndex, len = 0, status = 0;
+
+    /**
+     *
+     * @param stream
+     * @param randomizer Use {@link SecureRandom}
+     * @param sign
+     * @param signIndex
+     * @param dataMaxLength
+     */
+    public I4EOutputStream(final OutputStream stream, final Random randomizer, final byte[] sign, final int signIndex, final int dataMaxLength) {
+        this.dataMaxLength = dataMaxLength + 1;
+        this.stream = stream;
+        this.randomizer = randomizer;
+        this.sign = sign;
+        this.signIndex = signIndex;
+        this.randomData = new byte[dataMaxLength];
+    }
+
+    public I4EOutputStream(final OutputStream stream, final byte[] sign) { this(stream, new SecureRandom(), sign, 0, 128); }
+
+    private void internalWrite(int b) throws IOException {
+        final byte by = (byte) b;
+        if (signIndex >= sign.length)
+            signIndex = 0;
+        stream.write(by + sign[signIndex++]);
+    }
+
+    private void writeInt(final int n) throws IOException {
+        internalWrite(n >> 24);
+        internalWrite(n >> 16);
+        internalWrite(n >> 8);
+        internalWrite(n);
+    }
+
+    private void sendRandomPacket() throws IOException {
+        randomizer.nextBytes(randomData);
+        final int l = randomizer.nextInt(dataMaxLength);
+        writeInt(l);
+        stream.write(randomData, 0, l);
+    }
+
+    private void status() throws IOException {
+        if (status == 1) {
+            if (len > 0)
+                return;
+            sendRandomPacket();
+        }
+        sendRandomPacket();
+        len = randomizer.nextInt(dataMaxLength);
+        if (len == 0)
+            len++;
+        writeInt(len);
+        status = 1;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        status();
+        internalWrite(b);
+        len--;
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        status();
+        System.out.println(len + " / " + this.len);
+        while (len > this.len) {
+            stream.write(b, off, this.len);
+            off += this.len;
+            len -= this.len;
+            this.len = 0;
+            status();
+        }
+        stream.write(b, off, len);
+        this.len -= len;
+    }
+
+    @Override
+    public void flush() throws IOException {
+        stream.flush();
+    }
+
+    @Override
+    public void close() throws IOException {
+        stream.close();
+    }
+}
