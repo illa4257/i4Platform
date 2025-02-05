f583d67cdaa15808a692e9a1e77644cdbe4add4e
illa4257 <48959534+illa4257@users.noreply.github.com>
2025-02-05 11:39:37 -0800

    Refactor keyboard event handling.

--- i4Framework.swing/src/illa4257/i4Framework/swing/components/SwingComponent.java
+++ i4Framework.swing/src/illa4257/i4Framework/swing/components/SwingComponent.java
@@ -1,7 +1,7 @@
 package illa4257.i4Framework.swing.components;
 
 import illa4257.i4Framework.base.events.keyboard.KeyDownEvent;
-import illa4257.i4Framework.base.events.keyboard.KeyTypeEvent;
+import illa4257.i4Framework.base.events.keyboard.KeyPressEvent;
 import illa4257.i4Framework.base.events.keyboard.KeyUpEvent;
 import illa4257.i4Framework.base.styling.Cursor;
 import illa4257.i4Framework.base.events.EventListener;
@@ -18,6 +18,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 import static java.awt.Cursor.*;
 
@@ -81,18 +82,22 @@ public class SwingComponent extends JComponent implements ISwingComponent {
         addMouseWheelListener(event -> component.fire(new MouseScrollEvent(getGlobalX(event),
                 getGlobalY(event), event.getX(), event.getY(), event.getUnitsToScroll())));
         addKeyListener(new KeyListener() {
+            private final ArrayList<Integer> pressed = new ArrayList<>();
+
+            @Override public void keyTyped(final KeyEvent e) {}
+
             @Override
-            public void keyTyped(KeyEvent e) {
-                component.fire(new KeyTypeEvent(e.getKeyCode(), e.getKeyChar()));
+            public void keyPressed(final KeyEvent e) {
+                if (!pressed.contains(e.getKeyCode())) {
+                    pressed.add(e.getKeyCode());
+                    component.fire(new KeyDownEvent(e.getKeyCode(), e.getKeyChar()));
+                }
+                component.fire(new KeyPressEvent(e.getKeyCode(), e.getKeyChar()));
             }
 
             @Override
-            public void keyPressed(KeyEvent e) {
-                component.fire(new KeyDownEvent(e.getKeyCode(), e.getKeyChar()));
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
+            public void keyReleased(final KeyEvent e) {
+                pressed.remove((Object) e.getKeyCode());
                 component.fire(new KeyUpEvent(e.getKeyCode(), e.getKeyChar()));
             }
         });
