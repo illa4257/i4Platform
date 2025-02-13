e5efd16abe0eb7eff52c8a247c43607cfa54d014
illa4257 <48959534+illa4257@users.noreply.github.com>
2025-02-12 22:32:37 -0800

    Added arcTo and x/y getters to IPath.

--- i4Framework.swing/src/illa4257/i4Framework/swing/SwingPath.java
+++ i4Framework.swing/src/illa4257/i4Framework/swing/SwingPath.java
@@ -6,20 +6,29 @@ import java.awt.geom.Path2D;
 
 public class SwingPath implements IPath {
     public final Path2D path = new Path2D.Float();
+    private float x, y;
+
+    @Override public float x() { return x; }
+    @Override public float y() { return y; }
 
     @Override
     public void begin(final float x, final float y) {
-        path.moveTo(x, y);
+        path.moveTo(this.x = x, this.y = y);
     }
 
     @Override
     public void lineTo(final float x, final float y) {
-        path.lineTo(x, y);
+        path.lineTo(this.x = x, this.y = y);
     }
 
     @Override
     public void curveTo(final float x1, final float y1, final float x2, final float y2, final float x3, final float y3) {
-        path.curveTo(x1, y1, x2, y2, x3, y3);
+        path.curveTo(x1, y1, x2, y2, x = x3, y = y3);
+    }
+
+    @Override
+    public void curveTo(final float x1, final float y1, final float x2, final float y2) {
+        path.quadTo(x1, y1, x = x2, y = y2);
     }
 
     @Override
--- i4Framework/src/illa4257/i4Framework/base/graphics/IPath.java
+++ i4Framework/src/illa4257/i4Framework/base/graphics/IPath.java
@@ -1,10 +1,28 @@
 package illa4257.i4Framework.base.graphics;
 
 public interface IPath {
+    float x();
+    float y();
+
     void begin(final float x, final float y);
     default void begin() { begin(0, 0); }
     void lineTo(final float x, final float y);
     void curveTo(final float x1, final float y1, final float x2, final float y2, final float x3, final float y3);
     default void curveTo(final float x1, final float y1, final float x2, final float y2) { curveTo(x1, y1, x1, y1, x2, y2); }
+
+    default void arcTo(final float x, final float y, double startAngle, final double angle) {
+        float sx = x(), sy = y(), dx = x - sx, dy = y - sy;
+
+        final double len = Math.sqrt(dx * dx + dy * dy) * angle, step = angle / len;
+
+        if ((dx > 0 && dy > 0) || (dx < 0 && dy < 0)) {
+            startAngle -= 1.57;
+            for (double r = 0; r < angle; r += step)
+                lineTo(sx + (float) Math.cos(-startAngle - r) * dx, y - (float) Math.sin(-startAngle - r) * dy);
+        } else
+            for (double r = 0; r < angle; r += step)
+                lineTo(x - (float) Math.cos(startAngle + r) * dx, sy + (float) Math.sin(startAngle + r) * dy);
+    }
+
     void close();
 }
