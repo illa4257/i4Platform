895b74754139d3b31c6e6dd6efd2651116f85c59
illa4257 <48959534+illa4257@users.noreply.github.com>
2025-03-14 12:42:10 -0700

    Simplify `ChangePointEvent

--- i4Framework/src/illa4257/i4Framework/base/events/components/ChangePointEvent.java
+++ i4Framework/src/illa4257/i4Framework/base/events/components/ChangePointEvent.java
@@ -6,6 +6,4 @@ import illa4257.i4Framework.base.events.SingleEvent;
 public class ChangePointEvent extends BaseEvent implements SingleEvent {
     public ChangePointEvent() {}
     public ChangePointEvent(final boolean isSystem) { super(isSystem); }
-
-    @Override public SingleEvent combine(final SingleEvent old) { return this; }
 }
