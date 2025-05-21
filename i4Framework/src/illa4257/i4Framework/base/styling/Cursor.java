71585a08dd31a0afedf445da76f73ddfa0915fe2
illa4257 <48959534+illa4257@users.noreply.github.com>
2025-05-21 14:37:25 -0700

    Adds resize cursor types.

--- i4Framework/src/illa4257/i4Framework/base/styling/Cursor.java
+++ i4Framework/src/illa4257/i4Framework/base/styling/Cursor.java
@@ -2,19 +2,41 @@ package illa4257.i4Framework.base.styling;
 
 public enum Cursor {
     DEFAULT,
-    TEXT;
+    TEXT,
+
+    E_RESIZE,
+    EW_RESIZE,
+    N_RESIZE,
+    NE_RESIZE,
+    NS_RESIZE,
+    NW_RESIZE,
+    NWSE_RESIZE,
+    S_RESIZE,
+    SE_RESIZE,
+    SW_RESIZE,
+    W_RESIZE;
 
     Cursor() {}
 
     public static Cursor fromName(final String name) {
         if (name == null)
             return DEFAULT;
-        if (name.equalsIgnoreCase("text"))
-            return TEXT;
+        switch (name.replaceAll("_", "-").toLowerCase()) {
+            case "text": return TEXT;
+            case "e-resize": return E_RESIZE;
+            case "ew-resize": return EW_RESIZE;
+            case "n-resize": return N_RESIZE;
+            case "ne-resize": return NE_RESIZE;
+            case "ns-resize": return NS_RESIZE;
+            case "nw-resize": return NW_RESIZE;
+            case "nwse-resize": return NWSE_RESIZE;
+            case "s-resize": return S_RESIZE;
+            case "se-resize": return SE_RESIZE;
+            case "sw-resize": return SW_RESIZE;
+            case "w-resize": return W_RESIZE;
+        }
         return DEFAULT;
     }
 
-    public static Cursor from(final StyleSetting setting) {
-        return fromName(setting.get(String.class));
-    }
+    public static Cursor from(final StyleSetting setting) { return fromName(setting.get(String.class)); }
 }
