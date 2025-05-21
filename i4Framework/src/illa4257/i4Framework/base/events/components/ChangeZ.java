74568bd10b43fbde4129e618dfeb6ead4370bf18
illa4257 <48959534+illa4257@users.noreply.github.com>
2025-05-21 14:38:17 -0700

    Adds ChangeZ event for managing Z-axis changes.

---
+++ i4Framework/src/illa4257/i4Framework/base/events/components/ChangeZ.java
@@ -0,0 +1,16 @@
+package illa4257.i4Framework.base.events.components;
+
+import illa4257.i4Framework.base.events.SingleEvent;
+
+public class ChangeZ implements SingleEvent {
+    public boolean isPrevented = false, isParentPrevented = true;
+    public final int z;
+
+    public ChangeZ(final int z) { this.z = z; }
+
+    @Override public boolean isPrevented() { return isPrevented; }
+    @Override public boolean isParentPrevented() { return isParentPrevented || isPrevented; }
+
+    public ChangeZ prevent(final boolean prevent) { isPrevented = prevent; return this; }
+    public ChangeZ parentPrevent(final boolean prevent) { isParentPrevented = prevent; return this; }
+}
