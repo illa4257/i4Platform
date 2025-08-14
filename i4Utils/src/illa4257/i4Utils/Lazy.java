d8f7a6cea15702fa7c6f8b4c311f73e3e52b737f
illa4257 <48959534+illa4257@users.noreply.github.com>
2025-08-14 16:19:29 -0400

    Adds a thread-safe lazy initialization utility class.

---
+++ i4Utils/src/illa4257/i4Utils/Lazy.java
@@ -0,0 +1,24 @@
+package illa4257.i4Utils;
+
+import java.util.function.Supplier;
+
+public class Lazy<T> {
+    private final Supplier<T> supplier;
+    private volatile T value = null;
+
+    public Lazy(final Supplier<T> supplier) { this.supplier = supplier; }
+
+    public boolean isInitialized() { return value != null; }
+
+    public T get() {
+        T v = value;
+        if (v == null)
+            synchronized (this) {
+                if ((v = value) == null)
+                    value = v = supplier.get();
+            }
+        return v;
+    }
+
+    public void remove() { value = null; }
+}
