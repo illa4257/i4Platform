95d4bd9b44a707ef314432753c8a833eabc6a122
illa4257 <48959534+illa4257@users.noreply.github.com>
2025-09-28 23:54:39 -0400

    Android: Refactor border rendering. Focus support.

--- i4Framework.android/src/illa4257/i4Framework/android/AndroidView.java
+++ i4Framework.android/src/illa4257/i4Framework/android/AndroidView.java
@@ -8,11 +8,16 @@ import illa4257.i4Framework.base.components.Component;
 import illa4257.i4Framework.base.components.Container;
 import illa4257.i4Framework.base.events.EventListener;
 import illa4257.i4Framework.base.events.components.AddComponentEvent;
+import illa4257.i4Framework.base.events.components.FocusEvent;
 import illa4257.i4Framework.base.events.components.RecalculateEvent;
 import illa4257.i4Framework.base.events.components.RepaintEvent;
+import illa4257.i4Framework.base.math.Orientation;
+import illa4257.i4Framework.base.styling.StyleSetting;
 
 public class AndroidView extends ViewGroup {
     public final Component component;
+
+    private volatile int offset = 0;
 
     @SuppressWarnings({"FieldCanBeLocal", "unused"})
     private final EventListener<?>[] listeners;
@@ -24,6 +29,7 @@ public class AndroidView extends ViewGroup {
         setWillNotDraw(false);
         this.component = component;
         this.context = new AndroidGContext();
+        setFocusable(component.isFocusable());
         {
             final LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(component.width.calcInt(), component.height.calcInt());
             lp.leftMargin = component.startX.calcInt();
@@ -31,11 +37,16 @@ public class AndroidView extends ViewGroup {
             setLayoutParams(lp);
         }
         if (isNotRoot) {
-            layout(component.startX.calcInt(), component.startY.calcInt(), component.endX.calcInt(), component.endY.calcInt());
+            component.subscribe("border-width", this::updateLS);
+            component.subscribe("border-color", this::updateLS);
+            updateLS(null);
             listeners = new EventListener[] {
-                    component.addEventListener(RecalculateEvent.class, e ->
-                            layout(component.startX.calcInt(), component.startY.calcInt(), component.endX.calcInt(), component.endY.calcInt())),
-                    component.addEventListener(RepaintEvent.class, e -> invalidate())
+                    component.addEventListener(RecalculateEvent.class, e -> updateLS(null)),
+                    component.addEventListener(RepaintEvent.class, e -> invalidate()),
+                    component.addEventListener(FocusEvent.class, e -> {
+                        if (e.value)
+                            requestFocus();
+                    }),
             };
         } else
             listeners = new EventListener[0];
@@ -47,11 +58,18 @@ public class AndroidView extends ViewGroup {
         }
     }
 
+    private void updateLS(final StyleSetting ignored) {
+        final int bw = component.getColor("border-color").alpha > 0 ? Math.round(Math.max(component.calcStyleNumber("border-width", Orientation.HORIZONTAL, 0), 0)) : 0;
+        layout(component.startX.calcInt() - bw, component.startY.calcInt() - bw, component.endX.calcInt() + bw * 2, component.endY.calcInt() + bw * 2);
+        offset = bw;
+    }
+
     protected final AndroidGContext context;
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        canvas.translate(offset, offset);
         context.canvas = canvas;
         context.paint.setTextSize(19f * component.densityMultiplier.calcFloat() * getResources().getConfiguration().fontScale);
         component.paint(context);
