949fa4614559d1152fc5db4a26af1599e5d3edca
illa4257 <48959534+illa4257@users.noreply.github.com>
2026-01-01 20:17:50 -0500

    Fix HeightByContent.

--- i4Framework/src/illa4257/i4Framework/base/points/layout/HeightByContent.java
+++ i4Framework/src/illa4257/i4Framework/base/points/layout/HeightByContent.java
@@ -2,8 +2,8 @@ package illa4257.i4Framework.base.points.layout;
 
 import illa4257.i4Framework.base.components.Component;
 import illa4257.i4Framework.base.components.Container;
-import illa4257.i4Framework.base.events.Event;
 import illa4257.i4Framework.base.events.components.AddComponentEvent;
+import illa4257.i4Framework.base.events.components.RecalculateEvent;
 import illa4257.i4Framework.base.events.components.RemoveComponentEvent;
 import illa4257.i4Framework.base.points.Point;
 
@@ -29,17 +29,41 @@ public class HeightByContent extends Point {
         return my + value;
     }
 
-    private void clChange(final Event e) { reset(); }
+    private void r(final RecalculateEvent e) {
+        final Container c = e.component.getParent();
+        if (c != container)
+            return;
+        reset();
+    }
+
+    private void ac(final AddComponentEvent e) {
+        if (e.container != container)
+            return;
+        reset();
+        e.child.addEventListener(RecalculateEvent.class, this::r);
+    }
+
+    private void rc(final RemoveComponentEvent e) {
+        if (e.container != container)
+            return;
+        reset();
+        e.child.removeEventListener(this::r);
+    }
 
     @Override
     public void onConstruct() {
-        container.addEventListener(AddComponentEvent.class, this::clChange);
-        container.addEventListener(RemoveComponentEvent.class, this::clChange);
+        container.addEventListener(AddComponentEvent.class, this::ac);
+        container.addEventListener(RemoveComponentEvent.class, this::rc);
+        for (final Component c : container)
+            c.addEventListener(RecalculateEvent.class, this::r);
         super.onConstruct();
     }
 
     @Override
     public void onDestruct() {
-        container.removeEventListener(this::clChange);
+        container.removeEventListener(this::ac);
+        container.removeEventListener(this::rc);
+        for (final Component c : container)
+            c.removeEventListener(this::r);
     }
 }
