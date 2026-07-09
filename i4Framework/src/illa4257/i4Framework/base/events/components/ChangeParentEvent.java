c4362cdd77560d9329ae0b8b51cfcacae0594570
illa4257 <48959534+illa4257@users.noreply.github.com>
2026-07-09 10:19:09 -0400

    Stores old and new parent containers in ChangeParentEvent.

--- i4Framework/src/illa4257/i4Framework/base/events/components/ChangeParentEvent.java
+++ i4Framework/src/illa4257/i4Framework/base/events/components/ChangeParentEvent.java
@@ -1,10 +1,12 @@
 package illa4257.i4Framework.base.events.components;
 
 import illa4257.i4Framework.base.components.Component;
+import illa4257.i4Framework.base.components.Container;
 import illa4257.i4Framework.base.events.Event;
 import illa4257.i4Framework.base.events.SingleEvent;
 
 public class ChangeParentEvent extends Event implements SingleEvent {
-    public ChangeParentEvent(final Component component) { super(component); isParentPrevented = true; }
-    public ChangeParentEvent(final Component component, final boolean isSystem) { super(component, isSystem); isParentPrevented = true; }
+    public final Container oldValue, newValue;
+    public ChangeParentEvent(final Component component, final Container oldValue, final Container newValue) { super(component); this.oldValue = oldValue; this.newValue = newValue; isParentPrevented = true; }
+    public ChangeParentEvent(final Component component, final Container oldValue, final Container newValue, final boolean isSystem) { super(component, isSystem); this.oldValue = oldValue; this.newValue = newValue; isParentPrevented = true; }
 }
