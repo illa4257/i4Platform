b9d63c77b043596cb3ccce0ab1683d5378f1016b
illa4257 <48959534+illa4257@users.noreply.github.com>
2026-09-06 15:24:22 -0400

    Update AndroidImage.

--- i4Framework.android/src/illa4257/i4Framework/android/AndroidImage.java
+++ i4Framework.android/src/illa4257/i4Framework/android/AndroidImage.java
@@ -2,25 +2,31 @@ package illa4257.i4Framework.android;
 
 import android.graphics.Bitmap;
 import illa4257.i4Framework.base.graphics.Image;
-import illa4257.i4Framework.base.graphics.ImageBufferable;
+import illa4257.i4Framework.base.graphics.ImagePixelable;
 
-import java.nio.ByteBuffer;
-
-public class AndroidImage implements ImageBufferable {
+public class AndroidImage implements ImagePixelable {
     public Bitmap bitmap;
 
     public AndroidImage(final Bitmap bitmap) { this.bitmap = bitmap; }
 
-    @Override
+    /*@Override
     public ByteBuffer getByteBuffer() {
         final ByteBuffer b = ByteBuffer.allocate(bitmap.getRowBytes() * bitmap.getHeight());
         bitmap.copyPixelsToBuffer(b);
         return b;
-    }
+    }*/
 
     public static AndroidImage compute(final Image img) {
         final Bitmap b = Bitmap.createBitmap(img.width, img.height, Bitmap.Config.ARGB_8888);
         b.copyPixelsFromBuffer(img.asByteBuffer());
         return new AndroidImage(b);
+    }
+
+    @Override
+    public int[] getPixels() {
+        final int w = bitmap.getWidth(), h = bitmap.getHeight();
+        final int[] pixels = new int[w * h];
+        bitmap.getPixels(pixels, 0, w, 0, 0, w, h);
+        return pixels;
     }
 }
