92bbb32c6363e9ba05fb4d9e4e3afe28f50eb48d
illa4257 <48959534+illa4257@users.noreply.github.com>
2026-09-06 14:57:58 -0400

    Exports M class and its `ceilDiv` methods.

--- i4Utils/src/illa4257/i4Utils/math/M.java
+++ i4Utils/src/illa4257/i4Utils/math/M.java
@@ -1,11 +1,16 @@
 package illa4257.i4Utils.math;
 
+import illa4257.i4Utils.annotations.Export;
+
+@Export
 public class M {
+    @Export
     public static long ceilDiv(long x, long y) {
         final long q = x / y;
         return (x ^ y) >= 0 && (q * y != x) ? q + 1 : q;
     }
 
+    @Export
     public static long ceilDiv(long x, int y) {
         return ceilDiv(x, (long)y);
     }
